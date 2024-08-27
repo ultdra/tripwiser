b5846d7ecb890d1fa5881f51ce60dc5fd1d74ed1
Shu Hui <>
2024-08-28 02:23:33 +0800

    Added nodemon and body-parser middlewares

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,21 @@ const dotenv = require('dotenv');
 
 const router = require("./routes");
 
+//Middlewares
+const bodyParser = require('body-parser');
+
 // Loading the dotenv config files
 dotenv.config();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({urlencoded:true}));
+
 app.use("/", router);
+
+app.post("/tesPost", (req, res) => {
+  console.log(req.body);
+  res.send("Body received!");
+});
 
 const port = process.env.LOCALPORT || 3000;
 
