00b9840be7f79f52a0b6c9c67b06522c533ce65e
Shu Hui <>
2024-08-30 01:32:00 +0800

    Added finishing touches to google oauth verification

--- backend/Controller/auth.js
+++ backend/Controller/auth.js
@@ -1,5 +1,7 @@
 const passport = require('passport')
 const googleStrategy = require('passport-google-oauth20').Strategy
+const {OAuth2Client} = require('google-auth-library')
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
 //Strategy for validating google sign in
 passport.use(new googleStrategy({
@@ -8,6 +10,26 @@ passport.use(new googleStrategy({
     callbackURL: "http://localhost:8080/auth/googlecallback"
   }))
 
+export const ValidateGoogleToken = async (req,res) => {
+    console.log(req.body)
+    const {token} = req.body
+
+    try{
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.GOOGLE_CLIENT_ID
+          });
+          const payload = ticket.getPayload();
+          const userid = payload['sub'];
+          res.json({ userId: userid, email: payload['email'] });
+    }
+    catch(error)
+    {
+        console.error("Error verifying Google Token:", error)
+        res.status(401).json({error:'Invalid Token'})
+    }
+}
+
 passport.serializeUser((user, done) => {
     done(null, user)
 })
@@ -15,3 +37,4 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user,done) => {
     done(null, user)
 })
+
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,29 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {OAuth2Client} = require('google-auth-library')
 
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
+//Using functions from controller
+const {ValidateGoogleToken} = require('../Controller/auth')
 
-router.post('/validateGoogleAuth', async (req, res) => {
-    const {token} = req.body
-
-    try{
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: process.env.GOOGLE_CLIENT_ID
-          });
-          const payload = ticket.getPayload();
-          const userid = payload['sub'];
-          res.json({ userId: userid, email: payload['email'] });
-    }
-    catch(error)
-    {
-        console.error("Error verifying Google Token:", error)
-        res.status(401).json({error:'Invalid Token'})
-    }
-})
-
-router.
-
+router.post('/validateGoogleAuth', ValidateGoogleToken)
 
 module.exports = router;
